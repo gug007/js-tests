0e2741d19172fa2e7bc5fc9ebb1f16ca1a200792
gug007 <gug007@i.ua>
2017-01-16 13:48:05 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#JavaScript Questions
+What value is returned from the following statement?
+```javascript 
+    function f() { return this }
+    f.bind(6).bind(9)()
+   ``` 
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const fs = require('fs');
 const q = require('./src/questions');
 
 const readme = q.reduce((p, val) => {
-  p+=val.title;
+  p+=val.title + '\n';
   if(val.code) {
-    p+=`\r\n\`\`\`javascript ${val.code} \`\`\` \r\n`;
+    p+=`\`\`\`javascript ${val.code} \`\`\` \n`;
   }
   return p
-}, '');
+}, '#JavaScript Questions\n');
+
+fs.writeFile("README.md", readme, err => {
+  if(err) throw err;
+  console.log("The file was created!");
+});
 
 console.log(readme);
