141ca945f3d9ad1ae88920449f0de1aedf3faa85
Yury Tavakalyan <yury13t@mail.ru>
2017-01-17 11:16:26 +0200

    add question about (this2)

--- src/questions/index.js
+++ src/questions/index.js
@@ -31,6 +31,27 @@ const list = [{
     {answer: 'number\n undefined'},
     {answer: 'undefined\n undefined'}
   ]
+}, {
+  tags: ['es5'],
+  title: 'What will the following code output to the console?',
+  code: `
+    var spy = {
+      _name: 'James Bond',
+      getSecretName: function() {
+        return this._name;
+      }
+    };
+
+    var stoleSecretName = spy.getSecretName;
+
+    console.log(stoleSecretName(), spy.getSecretName());
+  `,
+  answers: [
+    {answer: 'undefined\n "James Bond"', isTrue: true},
+    {answer: '"James Bond"\n "James Bond"'},
+    {answer: 'undefined\n undefined'},
+    {answer: '"James Bond"\n undefined'}
+  ]
 }];
 
 module.exports = list;
