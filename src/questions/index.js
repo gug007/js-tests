0a13c940adfba680a08bb672db511754bef2b4ba
Yury Tavakalyan <yury13t@mail.ru>
2017-01-17 09:24:24 +0200

    add question about array

--- src/questions/index.js
+++ src/questions/index.js
@@ -31,6 +31,23 @@ const list = [{
     {answer: 'number\n undefined'},
     {answer: 'undefined\n undefined'}
   ]
+}, {
+  tags: ['es5'],
+  title: 'What will the code below output to the console?',
+  code: `
+    var arr1 = 'john'.split('');
+    var arr2 = arr1.reverse();
+    var arr3 = 'jones'.split('');
+    arr2.push(arr3);
+
+    console.log(arr1.slice(-1), arr2.slice(-1));
+  `,
+  answers: [
+    {answer: '[["j","o","n","e","s"]]\n [["j","o","n","e","s"]]', isTrue: true},
+    {answer: '["s"]\n ["s"]'},
+    {answer: '["j"]\n [["j","o","n","e","s"]]'},
+    {answer: '["j"]\n ["s"]'}
+  ]
 }];
 
 module.exports = list;
