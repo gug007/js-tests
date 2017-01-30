bdc31ed81543b3a22601e0e3628769440b180381
Yury <yury13t@mail.com>
2017-01-30 12:52:00 +0200

    edit little

--- dist/bundle.js
+++ dist/bundle.js
@@ -1091,19 +1091,19 @@ function createQuestion(questions, index = 0, userAnswers = [], userAnswer = '')
 
   const button = body(attr(elm('button'), {disabled: true}), 'Next');
 
-  const randomAnswers = getRandomData(questions[index].answers, 4);
+  const randomAnswers = getRandomArr(questions[index].answers, questions[index].answers.length);
   
   randomAnswers.forEach((v, i) => {
-    const input = attr(elm('input'), {id: (v.isTrue) ? v.isTrue : v.answer, type: 'radio', name: 'answer', value: v.answer});
+    const input = attr(elm('input'), {id: (v.isTrue) ? v.isTrue : i, type: 'radio', name: 'answer', value: v.answer});
     input.onchange = () => {
       if(userAnswer) { userAnswers.length = userAnswers.length - 1; }
       const correctAnswer = section.querySelector('#true').value;
-      userAnswer = section.querySelector('input[type="radio"]:checked').value;
-      userAnswers.push(userAnswer === correctAnswer ? true : false);
+      userAnswer = section.querySelector('input:checked').value;
+      userAnswers.push(userAnswer === correctAnswer);
       button.disabled = false;
     };
     const label = body(elm('label'), v.answer);
-    label.setAttribute('for', (v.isTrue) ? v.isTrue : v.answer);    
+    label.setAttribute('for', (v.isTrue) ? v.isTrue : i);    
     
     addChild(section, input);
     addChild(section, label);
@@ -1125,6 +1125,7 @@ function createQuestion(questions, index = 0, userAnswers = [], userAnswer = '')
 }
 
 function showResult(userAnswers) {
+  console.log(userAnswers);
   const numOfCorrAns = userAnswers.filter((v) => v === true).length;
   const percentages = numOfCorrAns / userAnswers.length * 100;
   const scaleFive = 5 * percentages / 100;
@@ -1136,7 +1137,7 @@ function showResult(userAnswers) {
   addChild(root, div);
 }
 
-function getRandomData(arr, num) {
+function getRandomArr(arr, num) {
   const randomData = [];
   const history = {};
 
@@ -1153,7 +1154,7 @@ function getRandomData(arr, num) {
   return randomData;
 }
 
-createQuestion(getRandomData(__WEBPACK_IMPORTED_MODULE_0__questions__["a" /* default */], 5));
+createQuestion(getRandomArr(__WEBPACK_IMPORTED_MODULE_0__questions__["a" /* default */], 5));
 
 
 /*const elm = (type, props) => {
